09b18cb2511c8b8130c995ce98f38b15c66f1d45
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-09-22 19:51:10 +0800

    There were a lot changes

---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories;
+
+[BindProperties]
+public class EditModel : PageModel
+{
+    private readonly AppDbContext _appDbContext;
+
+    public Category Category { get; set; }
+    public EditModel(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public void OnGet(int? id)
+    {
+        if (id is not null and not 0)
+        {
+            Category = _appDbContext.Categories.FirstOrDefault(c => c.Id == id);
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            _appDbContext.Categories.Update(Category!);
+            _appDbContext.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToPage("index");
+        }
+
+        return Page();
+    }
+}
