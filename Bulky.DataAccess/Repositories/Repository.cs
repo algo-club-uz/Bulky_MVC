882bbfedb1600e55be2b9e2e8c9b4f6dad0f7ee8
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-09-23 19:30:01 +0800

    IRepository was added with implementation

---
+++ Bulky.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Bulky.DataAccess.Repositories.IRepositories;
+using System.Linq.Expressions;
+using Bulky.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _appDbContext;
+    internal DbSet<T> dbSet;
+    public Repository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+        dbSet = _appDbContext.Set<T>();
+    }
+
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+        
+    }
+
+    public T Get(Expression<Func<T, bool>> filter)
+    {
+        IQueryable<T> query = dbSet;
+        query = query.Where(filter);
+
+        return query.FirstOrDefault()!;
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        IQueryable<T> query = dbSet;
+
+        return query.ToList();
+    }
+
+    public void Remove(T entity)
+    {
+        dbSet.Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        dbSet.RemoveRange(entities);
+    }
+}
