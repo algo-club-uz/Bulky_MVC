08100826467df4faf0cc08325fe486e99c2efd3c
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-07 17:41:18 +0800

    OrderController and OrderMV were added

---
+++ Bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,7 @@
+﻿namespace Bulky.Models.ViewModels;
+
+public class OrderVM
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetails { get; set; }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Bulky.DataAccess.Repositories;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly UnitOfWork _unitOfWork;
+
+    public OrderController(UnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<OrderHeader> orderHeaders = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser").ToList();
+        return Json(new { data = orderHeaders });
+    }
+
+    #endregion
+
+}
