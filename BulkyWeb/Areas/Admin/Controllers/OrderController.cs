c4ab466f76800f8da4ef55f0af5afef8b39dbe1d
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-10-14 15:50:46 +0800

    Demo Filters in Order List

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Bulky.DataAccess.Repositories.IRepositories;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Bulky.Utility;
 
 namespace BulkyWeb.Areas.Admin.Controllers;
 
@@ -22,9 +24,28 @@ public class OrderController : Controller
     #region API CALLS
 
     [HttpGet]
-    public IActionResult GetAll()
+    public IActionResult GetAll(string status)
     {
-        List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser").ToList();
+        IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser").ToList();
+
+        switch (status)
+        {
+            case "pending":
+                objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
+                break;
+            case "completed":
+                objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
+                break;
+            case "approved":
+                objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
+                break;
+            default:
+                break;
+        }
+
         return Json(new { data = objOrderHeaders });
     }
 
